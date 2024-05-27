4263dca52cb4967efa0544439fcf9c1bffaf4d87
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-05-27 13:21:11 +0530

    Add initial project files and structure

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/AnnalingamGowsigan"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/annalingam-gowsigan-87a3991b4/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://people.ce.pdn.ac.lk/students/e18/115/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-dribbble"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
